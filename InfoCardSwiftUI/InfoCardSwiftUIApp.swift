81b7a9c236484884cb1b8ed8842d7827f27f5b68
tahaturan <tahaaturann16@gmail.com>
2023-08-31 01:03:56 +0300

    Initial Commit

---
+++ InfoCardSwiftUI/InfoCardSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfoCardSwiftUIApp.swift
+//  InfoCardSwiftUI
+//
+//  Created by Taha Turan on 31.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct InfoCardSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
